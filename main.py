3a8b96b60e9eec3a7367d09ebef155279a325b00
serafim7k <mycomputer10j12@gmail.com>
2024-10-13 19:01:45 +0300

    django

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
-list_fruits = ["lemon", "orange", "apple", "kiwi"]
+newDict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
 
-[list_fruits.append("mango") for i in list_fruits if i == "kiwi"]
+print(newDict)
 
-print(list_fruits)
+print(newDict["brand"])
+
+newDict["year"] = 2018
+
+print(newDict)
+
+newDict["color"] = "red"
+print(newDict)
+
+newDict.pop("model")
+print(newDict)
+
+otherDict = {}
+
+otherDict = {i: i for i in range(10)}
+
+print(otherDict)
